53d9ce5eae0602f087a3c1c7e36aac3df1dab9ad
GregoryGromov <152792317+GregoryGromov@users.noreply.github.com>
2024-07-03 14:49:25 +0300

    Update DataManager.swift

--- Combine+2VM/Combine+2VM/Root/DataManager.swift
+++ Combine+2VM/Combine+2VM/Root/DataManager.swift
@@ -1,12 +1,4 @@
-//
-//  DataManager.swift
-//  Combine+2VM
-//
-//  Created by Григорий Громов on 03.07.2024.
-//
-
 import Foundation
-
 import Combine
 
 
@@ -20,24 +12,3 @@ class DataManager {
     }
 }
 
-
-
-//class DataManager {
-//    static let shared = DataManager()
-//    
-//    private var cancellables = Set<AnyCancellable>()
-//    
-//    @Published var text = "Hello, I am your destiny"
-//    
-//    private init() {
-//        $text
-//            .sink { newText in
-//                print("DataManager: Text changed to \(newText)")
-//            }
-//            .store(in: &cancellables)
-//    }
-//    
-//    func updateText(_ newText: String) {
-//        text = newText
-//    }
-//}
